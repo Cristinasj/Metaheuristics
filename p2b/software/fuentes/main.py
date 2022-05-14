109fccfd69bf0520365d41cd3edf264050f84063
Cristinasj <cristinasj@protonmail.com>
2022-05-14 18:20:56 +0200

    to laptop

--- p2b/software/fuentes/main.py
+++ p2b/software/fuentes/main.py
@@ -2,5 +2,20 @@ import blx
 import agg
 from scipy.io import arff
 
-# Lectura de datos 
-data = arff.loadarff()
+# Lectura de datos
+print ("¿Qué datos quiere leer?")
+print("1- ionosphere")
+print("2- parkinsons")
+print("3- spectf-heart")
+i = input("Introduzca 1, 2 o 3")
+while i != 1 or i != 2 or i!= 3: 
+    i = input("Pruebe otra vez") 
+
+if (i == 1): 
+    archivo = "../bin/datos/Instancias_APC/ionosphere.arff"
+if (i == 2): 
+    archivo = "../bin/datos/Instancias_APC/parkinsons.arff"
+if (i == 3): 
+    archivo = "../bin/datos/Instancias_APC/speftf-heart.arff"
+
+data = arff.loadarff(archivo)
